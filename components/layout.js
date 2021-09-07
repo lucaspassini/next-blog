b4c3ff8fc6633f7caa78ccca42773e460b592ce0
Lucas Cristiano Passini <lucas.passini1@gmail.com>
2021-09-07 19:49:58 -0300

    added dynamic routes and api routes

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,11 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from './layout.module.scss'
-import utilStyles from '../styles/utils.module.scss'
-import Link from 'next/link'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-const name = 'Lucas C. Passini'
-export const siteTitle = "Lucas's Blog"
+const name = "Lucas Cristiano Passini";
+export const siteTitle = "Lucas's Next.js Blog";
 
 export default function Layout({ children, home }) {
   return (
@@ -69,5 +69,5 @@ export default function Layout({ children, home }) {
         </div>
       )}
     </div>
-  )
+  );
 }
